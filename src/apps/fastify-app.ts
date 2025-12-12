68778a5b9690a81e9ebf5c5b621636f35caa2f83
mariol03 <mario.laglerabarco@gmail.com>
2025-12-12 19:56:48 +0100

    Add .env to .gitignore and update Fastify app routes and Swagger configuration
    Add .env to .gitignore to prevent sensitive environment variables from
    being committed.
    
    Update Fastify app to include item routes and adjust Swagger
    configuration for consistency and clarity.

--- src/apps/fastify-app.ts
+++ src/apps/fastify-app.ts
@@ -2,68 +2,70 @@ import fastify from "fastify";
 import { healthRoutes } from "../contexts/shared/infrastructure/health.fastify-route";
 import { authRoutes } from "../contexts/users/infrastructure/auth.fastify-route";
 import { userRoutes } from "../contexts/users/infrastructure/user.fastify-route";
-import swagger from '@fastify/swagger';
-import swaggerUi from '@fastify/swagger-ui';
-import { swaggerOptions } from '../openapi';
-import cors from '@fastify/cors';
+import swagger from "@fastify/swagger";
+import swaggerUi from "@fastify/swagger-ui";
+import { swaggerOptions } from "../openapi";
+import cors from "@fastify/cors";
+import { itemRoutes } from "@/contexts/items/infrastructure/item.fastify-route";
 
 // registrar plugins y rutas
 export const app = fastify({
     logger: {
-    transport: {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:standard',
-        ignore: 'pid,hostname',
-      },
+        transport: {
+            target: "pino-pretty",
+            options: {
+                colorize: true,
+                translateTime: "SYS:standard",
+                ignore: "pid,hostname",
+            },
+        },
     },
-  },
 });
 
 // Configurar CORS
 app.register(cors, {
-  origin: (origin, cb) => {
-    const allowedOrigins = [
-      'http://localhost:3000',
-      'http://localhost:3001',
-    ];
+    origin: (origin, cb) => {
+        const allowedOrigins = [
+            "http://localhost:3000",
+            "http://localhost:3001",
+        ];
 
-    if (!origin || allowedOrigins.includes(origin)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Origen no permitido'), false);
-    }
-  },
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  credentials: true, // Permitir cookies y encabezados de autenticación
+        if (!origin || allowedOrigins.includes(origin)) {
+            cb(null, true);
+        } else {
+            cb(new Error("Origen no permitido"), false);
+        }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // Permitir cookies y encabezados de autenticación
 });
 
 // Registrar OpenAPI/Swagger
 app.register(swagger as any, swaggerOptions as any);
 app.register(swaggerUi as any, {
-  routePrefix: '/docs',
-  uiConfig: {
-    docExpansion: 'list',
-  },
-  staticCSP: true,
-  transformStaticCSP: (header: string) => header,
-  swagger: {
-    url: '/openapi.json',
-  },
+    routePrefix: "/docs",
+    uiConfig: {
+        docExpansion: "list",
+    },
+    staticCSP: true,
+    transformStaticCSP: (header: string) => header,
+    swagger: {
+        url: "/openapi.json",
+    },
 });
 
 // Exponer especificación OpenAPI en JSON
-app.get('/openapi.json', async (request, reply) => {
-  // @ts-ignore
-  return app.swagger();
+app.get("/openapi.json", async (request, reply) => {
+    // @ts-except-error
+    return app.swagger();
 });
 
 app.register(healthRoutes, { prefix: "/health" });
 app.register(authRoutes, { prefix: "/auth" });
-app.register(userRoutes, { prefix: "/users" });
+// app.register(userRoutes, { prefix: "/users" });
+app.register(itemRoutes, { prefix: "/items" });
 
 // Manejo de rutas no encontradas (404)
 app.setNotFoundHandler((request, reply) => {
-  reply.status(404).send({ message: 'Endpoint not found' });
+    reply.status(404).send({ message: "Endpoint not found" });
 });
