1bc14871d052de15c5f6a11ec87180f509ecff58
mariol03 <mario.laglerabarco@gmail.com>
2025-12-16 20:18:10 +0100

    Remove wishlist module, to implemtent later and related files

--- src/contexts/items/wishlist/domain/wishlist.repository.ts
+++
@@ -1,8 +0,0 @@
-import { WishlistEntity } from './wishlist.entity';
-
-export interface WishlistRepository {
-  create(wishlist: WishlistEntity): Promise<WishlistEntity>;
-  update(wishlist: WishlistEntity): Promise<WishlistEntity>;
-  delete(wishlist: WishlistEntity): Promise<void>;
-  findByOwnerId(ownerId: string): Promise<WishlistEntity | null>;
-}
