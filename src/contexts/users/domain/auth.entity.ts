b50cf55f834f112cba3ff02c038c4f8f183e839c
mariol03 <mario.laglerabarco@gmail.com>
2025-12-20 17:54:43 +0100

    Remove AuthResponseDto and add image property to UserEntity

--- src/contexts/users/domain/auth.entity.ts
+++
@@ -1,9 +0,0 @@
-import { UserRole } from "./user.entity";
-
-export interface AuthResponseDto {
-    id: string;
-    name: string;
-    email: string;
-    role: UserRole;
-    token: string;
-}
--- src/contexts/users/domain/user.entity.ts
+++ src/contexts/users/domain/user.entity.ts
@@ -1,64 +1,61 @@
-// entidad de usuario
-// src/contexts/users/user.entity.ts
 import { BaseEntity } from "@shared/base.entity";
 
 export enum UserRole {
-  BUYER = "buyer",
-  SELLER = "seller"
+    BUYER = "buyer",
+    SELLER = "seller",
 }
 
 export class UserEntity extends BaseEntity {
-  private _name!: string;
-  private _email!: string;
-  private _password!: string; // Hash de la contraseña
-  private _role: UserRole = UserRole.BUYER;
-  private _isVerified: boolean = false; // Por defecto, el usuario no está verificado
-  
-  constructor(name: string, email: string, password: string, role: UserRole = UserRole.BUYER) {
-    super(); // Llama al constructor de la clase base
-    this._name = name;
-    this._email = email;
-    this._password = password;
-    this._role = role;
-  }
+    private _name!: string;
+    private _email!: string;
+    private _password!: string; // Hash de la contraseña
+    private _image!: string | null;
+    private _role: UserRole = UserRole.BUYER;
 
-  get name(): string {
-    return this._name;
-  }
+    constructor(
+        name: string,
+        email: string,
+        password: string,
+        role: UserRole = UserRole.BUYER,
+        image: string | null = null,
+    ) {
+        super(); // Llama al constructor de la clase base
+        this._name = name;
+        this._email = email;
+        this._password = password;
+        this._role = role;
+        this._image = image;
+    }
 
-  set name(value: string) {
-    this._name = value;
-  }
+    get name(): string {
+        return this._name;
+    }
 
-  get email(): string {
-    return this._email;
-  }
+    set name(value: string) {
+        this._name = value;
+    }
 
-  set email(value: string) {
-    this._email = value;
-  }
+    get email(): string {
+        return this._email;
+    }
 
-  get password(): string {
-    return this._password;
-  }
+    set email(value: string) {
+        this._email = value;
+    }
 
-  set password(value: string) {
-    this._password = value;
-  }
+    get password(): string {
+        return this._password;
+    }
 
-  get role(): UserRole {
-    return this._role;
-  }
+    set password(value: string) {
+        this._password = value;
+    }
 
-  set role(value: UserRole) {
-    this._role = value;
-  }
+    get role(): UserRole {
+        return this._role;
+    }
 
-  get isVerified(): boolean {
-    return this._isVerified;
-  }
-
-  set isVerified(value: boolean) {
-    this._isVerified = value;
-  }
+    set role(value: UserRole) {
+        this._role = value;
+    }
 }
