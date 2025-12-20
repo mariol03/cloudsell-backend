1ea78ba06e94e8de8d0bd70a2e733f76f99ba77f
mariol03 <mario.laglerabarco@gmail.com>
2025-12-20 17:49:13 +0100

    Remove un-used user CRUD use cases and DTOs

--- src/contexts/users/application/use-cases/create/dto/user-create.dto.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from "@users/domain/user.entity";
-
-export interface UserCreateDto {
-  name: string;
-  email: string;
-  password: string;
-  role?: UserRole;
-}
--- src/contexts/users/application/use-cases/delete/dto/user-delete.dto.ts
+++
@@ -1,3 +0,0 @@
-export interface UserDeleteDto {
-  id: string;
-}
