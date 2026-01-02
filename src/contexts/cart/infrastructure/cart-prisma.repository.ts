1cd57df013fc06e8b597bcdbf2aceaa24d99ec56
mariol03 <mario.laglerabarco@gmail.com>
2026-01-02 20:28:01 +0100

    fix: cart not clearing correctly

--- src/contexts/cart/infrastructure/cart-prisma.repository.ts
+++ src/contexts/cart/infrastructure/cart-prisma.repository.ts
@@ -1,4 +1,9 @@
-import { Cart, CartItem, Item, User } from "@/contexts/shared/infrastructure/prisma/client";
+import {
+    Cart,
+    CartItem,
+    Item,
+    User,
+} from "@/contexts/shared/infrastructure/prisma/client";
 import { CartEntity } from "../domain/cart.entity";
 import { CartRepository } from "../domain/cart.repository";
 import { prisma } from "../../shared/infrastructure/prisma-singletons";
@@ -15,6 +20,31 @@ type CartWithRelations = Cart & {
 
 export class CartPrismaRepository implements CartRepository {
     async save(cart: CartEntity): Promise<CartEntity> {
+        // Obtener el carrito existente
+        const oldCart = await prisma.cart.findMany({
+            where: { userId: cart.userId },
+            select: {
+                id: true,
+                userId: true,
+                items: {
+                    select: {
+                        id: true,
+                    },
+                },
+            },
+        });
+
+        // si existe un carrito existente, eliminar los viejos items y insertar los nuevos
+        if (oldCart) {
+            for (const cart of oldCart) {
+                await prisma.cartItem.deleteMany({
+                    where: { cartId: cart.id },
+                });
+                await prisma.cart.delete({ where: { id: cart.id } });
+            }
+        }
+
+        // crear el nuevo carrito
         const newCart = await prisma.cart.create({
             data: {
                 userId: cart.userId,
@@ -39,6 +69,8 @@ export class CartPrismaRepository implements CartRepository {
                 },
             },
         });
+
+        // devolver el nuevo carrito creado
         return this.toCartEntity(newCart);
     }
     async findByUserId(userId: string): Promise<CartEntity | undefined> {
@@ -73,13 +105,16 @@ export class CartPrismaRepository implements CartRepository {
         const cartEntity = new CartEntity(cart.userId);
         cartEntity.id = cart.id;
         cartEntity.items = cart.items.map((i) => {
-            const sellerUserRole = i.item.seller.role === "seller" ? UserRole.SELLER : UserRole.BUYER;
+            const sellerUserRole =
+                i.item.seller.role === "seller"
+                    ? UserRole.SELLER
+                    : UserRole.BUYER;
             const seller = new UserEntity(
                 i.item.seller.name,
                 i.item.seller.email,
                 i.item.seller.password,
                 sellerUserRole,
-                i.item.seller.image
+                i.item.seller.image,
             );
             seller.id = i.item.seller.id;
 
@@ -89,7 +124,7 @@ export class CartPrismaRepository implements CartRepository {
                 i.item.image || "",
                 i.item.price,
                 i.item.stock,
-                seller
+                seller,
             );
             itemEntity.id = i.item.id;
 
